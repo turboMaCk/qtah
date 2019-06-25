c2699090d1a506862be32e86bb0e0dc5926a2747
juliagoda <juliagoda.de@gmail.com>
2019-06-25 14:16:03 +0200

    Updated QMetaMethod

--- qtah-generator/src/Graphics/UI/Qtah/Generator/Interface/Core/QMetaMethod.hs
+++ qtah-generator/src/Graphics/UI/Qtah/Generator/Interface/Core/QMetaMethod.hs
@@ -21,19 +21,27 @@ module Graphics.UI.Qtah.Generator.Interface.Core.QMetaMethod (
   ) where
 
 import Foreign.Hoppy.Generator.Spec (
-  Export (ExportClass),
+  Export (ExportClass, ExportEnum),
+  Operator (OpNe, OpEq),
   addReqIncludes,
   classSetConversionToGc,
   classSetEntityPrefix,
   ident,
+  ident1,
   includeStd,
   makeClass,
+  mkConstMethod,
+  mkMethod,
   )
 import Foreign.Hoppy.Generator.Spec.ClassFeature (
   ClassFeature (Copyable),
   classAddFeatures,
   )
---import Foreign.Hoppy.Generator.Types (boolT, intT, objT, ptrT, refT, voidT)
+import Foreign.Hoppy.Generator.Types (boolT, intT, objT, ptrT, refT, voidT, enumT, constT, charT)
+import Foreign.Hoppy.Generator.Version (collect, just, test)
+import Graphics.UI.Qtah.Generator.Flags (qtVersion)
+import Graphics.UI.Qtah.Generator.Interface.Core.QByteArray (c_QByteArray)
+import {-# SOURCE #-} Graphics.UI.Qtah.Generator.Interface.Core.QList (c_QListQByteArray)
 import Graphics.UI.Qtah.Generator.Module (AModule (AQtModule), makeQtModule)
 import Graphics.UI.Qtah.Generator.Types
 
@@ -42,13 +50,49 @@ import Graphics.UI.Qtah.Generator.Types
 aModule =
   AQtModule $
   makeQtModule ["Core", "QMetaMethod"]
-  [ QtExport $ ExportClass c_QMetaMethod ]
+  [ QtExport $ ExportClass c_QMetaMethod
+  , QtExport $ ExportEnum e_Access
+  , QtExport $ ExportEnum e_MethodType ]
 
 c_QMetaMethod =
   addReqIncludes [includeStd "QMetaMethod"] $
   classSetConversionToGc $
   classAddFeatures [Copyable] $
   classSetEntityPrefix "" $
-  makeClass (ident "QMetaMethod") Nothing []
-  [ -- TODO
+  makeClass (ident "QMetaMethod") Nothing [] $
+  collect
+  [ just $ mkConstMethod "access" [] $ enumT e_Access
+  , test (qtVersion >= [5, 0]) $ mkConstMethod "isValid" [] boolT
+  , test (qtVersion >= [4, 6]) $ mkConstMethod "methodIndex" [] intT
+  , test (qtVersion >= [5, 0]) $ mkConstMethod "methodSignature" [] $ objT c_QByteArray
+  , just $ mkConstMethod "methodType" [] $ enumT e_MethodType
+  , test (qtVersion >= [5, 0]) $ mkConstMethod "name" [] $ objT c_QByteArray
+  , test (qtVersion >= [5, 0]) $ mkConstMethod "parameterCount" [] intT
+  , just $ mkConstMethod "parameterNames" [] $ objT c_QListQByteArray
+  , test (qtVersion >= [5, 0]) $ mkConstMethod "parameterType" [intT] intT
+  , just $ mkConstMethod "parameterTypes" [] $ objT c_QListQByteArray
+  , test (qtVersion >= [5, 0]) $ mkConstMethod "returnType" [] intT
+  , test (qtVersion >= [5, 1]) $ mkConstMethod "revision" [] intT
+  , just $ mkConstMethod "tag" [] $ ptrT $ constT charT
+  , just $ mkConstMethod "typeName" [] $ ptrT $ constT charT
+  , test (qtVersion >= [5, 0]) $ mkMethod OpNe [refT $ constT $ objT c_QMetaMethod] boolT
+  , test (qtVersion >= [5, 0]) $ mkMethod OpEq [refT $ constT $ objT c_QMetaMethod] boolT
+
+  -- TODO invoke methods
+  ]
+
+  
+e_Access =
+  makeQtEnum (ident1 "QMetaMethod" "Access") [includeStd "QMetaMethod"]
+  [ (0, ["private"])
+  , (1, ["protected"])
+  , (2, ["public"])
+  ]
+
+e_MethodType =
+  makeQtEnum (ident1 "QMetaMethod" "MethodType") [includeStd "QMetaMethod"]
+  [ (0, ["method"])
+  , (1, ["signal"])
+  , (2, ["slot"])
+  , (3, ["constructor"])
   ]
