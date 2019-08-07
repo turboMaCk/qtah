3ed7b8834d5cbbd9ac25f148b99513c3f5a2564c
juliagoda <juliagoda.de@gmail.com>
2019-08-07 19:24:23 +0200

    Updated QMessageLogger

--- qtah-generator/src/Graphics/UI/Qtah/Generator/Interface/Core/QMessageLogger.hs
+++ qtah-generator/src/Graphics/UI/Qtah/Generator/Interface/Core/QMessageLogger.hs
@@ -4,9 +4,11 @@ module Graphics.UI.Qtah.Generator.Interface.Core.QMessageLogger (
   ) where
 
 
-
 import Foreign.Hoppy.Generator.Spec (
   Export (ExportClass),
+  Type,
+  makeCallback,
+  toExtName,
   addReqIncludes,
   classSetEntityPrefix,
   ident,
@@ -25,14 +27,13 @@ import Foreign.Hoppy.Generator.Spec (
   mkMethod
   )
   
-
-import Foreign.Hoppy.Generator.Types (charT, intT, voidT, enumT, bitspaceT, constT, objT, ptrT, refT)
+import Foreign.Hoppy.Generator.Types (callbackT, charT, intT, voidT, enumT, bitspaceT, constT, objT, ptrT, refT, fnT)
 import Foreign.Hoppy.Generator.Version (collect, just, test)
 import Graphics.UI.Qtah.Generator.Flags (qtVersion)
 import Graphics.UI.Qtah.Generator.Module (AModule (AQtModule), makeQtModuleWithMinVersion)
 import Graphics.UI.Qtah.Generator.Interface.Core.QDebug (c_QDebug)
+import Graphics.UI.Qtah.Generator.Interface.Core.QLoggingCategory (c_QLoggingCategory)
 import Graphics.UI.Qtah.Generator.Types
-
 
 {-# ANN module "HLint: ignore Use camelCase" #-}
 
@@ -53,25 +54,26 @@ c_QMessageLogger =
   , just $ mkCtor "newShort" [ptrT $ constT $ charT, intT, ptrT $ constT $ charT]
   , just $ mkCtor "new" []
   , just $ mkConstMethod' "critical" "critical" [ptrT $ constT charT] voidT
-  --, test (qtVersion >= [5, 3]) $ mkConstMethod' "critical" "criticalWithQLoggingChar" [refT $ constT $ objT c_QLoggingCategory, ptrT $ constT charT] voidT
-  --, test (qtVersion >= [5, 3]) $ mkConstMethod "critical" [enumT e_CategoryFunction, ptrT $ constT charT] voidT
-  , just $ mkConstMethod' "critical" "criticalWithQDebug" [] $ objT c_QDebug
-  --, test (qtVersion >= [5, 3]) $ mkConstMethod' "critical" "criticalWithQLogging" [refT $ constT $ objT c_QLoggingCategory] $ objT c_QDebug
-  --, test (qtVersion >= [5, 3]) $ mkConstMethod' "critical" "criticalWithCategory" [enumT e_CategoryFunction] $ objT c_QDebug
+  , test (qtVersion >= [5, 3]) $ mkConstMethod' "critical" "criticalWithQLoggingChar" [refT $ constT $ objT c_QLoggingCategory, ptrT $ constT charT] voidT
+  --, test (qtVersion >= [5, 3]) $ mkConstMethod' "critical" "criticalWithCategoryFunc" [categoryFunction, ptrT $ constT charT] voidT
+  , just $ mkConstMethod' "critical" "criticalQDebug" [] $ objT c_QDebug
+  , test (qtVersion >= [5, 3]) $ mkConstMethod' "critical" "criticalWithQLogging" [refT $ constT $ objT c_QLoggingCategory] $ objT c_QDebug
+  --, test (qtVersion >= [5, 3]) $ mkConstMethod' "critical" "criticalWithCategoryDebug" [categoryFunction] $ objT c_QDebug
   , just $ mkConstMethod' "debug" "debugWithChar" [ptrT $ constT charT] voidT
-  --, test (qtVersion >= [5, 3]) $ mkConstMethod' "debug" "debugWithQLoggingChar" [refT $ constT $ objT c_QLoggingCategory, ptrT $ constT charT] voidT
-  --, test (qtVersion >= [5, 3]) $ mkConstMethod' "debug" "debugWithCategory" [enumT e_CategoryFunction, ptrT $ constT charT] voidT
+  , test (qtVersion >= [5, 3]) $ mkConstMethod' "debug" "debugWithQLoggingChar" [refT $ constT $ objT c_QLoggingCategory, ptrT $ constT charT] voidT
+  --, test (qtVersion >= [5, 3]) $ mkConstMethod' "debug" "debugWithCategoryChar" [categoryFunction, ptrT $ constT charT] voidT
   , just $ mkConstMethod' "debug" "debug" [] $ objT c_QDebug
   , just $ mkConstMethod' "fatal" "fatalWithChar" [ptrT $ constT charT] voidT
   , test (qtVersion >= [5, 5]) $ mkConstMethod' "info" "infoWithChar" [ptrT $ constT charT] voidT
-  --, test (qtVersion >= [5, 5]) $ mkConstMethod' "info" "infoWithQLoggingChar" [refT $ constT $ objT c_QLoggingCategory, ptrT $ constT charT] voidT
-  --, test (qtVersion >= [5, 5]) $ mkConstMethod' "info" "infoWithCategory" [enumT e_CategoryFunction, ptrT $ constT charT] voidT
+  , test (qtVersion >= [5, 5]) $ mkConstMethod' "info" "infoWithQLoggingChar" [refT $ constT $ objT c_QLoggingCategory, ptrT $ constT charT] voidT
+  --, test (qtVersion >= [5, 5]) $ mkConstMethod' "info" "infoWithCategory" [categoryFunction, ptrT $ constT charT] voidT
   , test (qtVersion >= [5, 5]) $ mkConstMethod' "info" "info" [] $ objT c_QDebug
   , just $ mkConstMethod' "warning" "warningWithChar" [ptrT $ constT charT] voidT
-  --, test (qtVersion >= [5, 3]) $ mkConstMethod' "warning" "warningWithQLoggingChar" [refT $ constT $ objT c_QLoggingCategory, ptrT $ constT charT] voidT
-  --, test (qtVersion >= [5, 3]) $ mkConstMethod' "warning" "warningWithCategory" [enumT e_CategoryFunction, ptrT $ constT charT] voidT
+  , test (qtVersion >= [5, 3]) $ mkConstMethod' "warning" "warningWithQLoggingChar" [refT $ constT $ objT c_QLoggingCategory, ptrT $ constT charT] voidT
+  --, test (qtVersion >= [5, 3]) $ mkConstMethod' "warning" "warningWithCategory" [categoryFunction, ptrT $ constT charT] voidT
   , test (qtVersion >= [5, 3]) $ mkConstMethod' "warning" "warning" [] $ objT c_QDebug
   ]
   
   
-
+--categoryFunction :: Type
+--categoryFunction = callbackT $ makeCallback (toExtName "category") [] $ refT $ constT $ objT c_QLoggingCategory
